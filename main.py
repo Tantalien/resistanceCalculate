61c9238cdfa63c955a5667f0a33e06c6325c5af5
Elena <123@123321.ru>
2023-06-11 17:15:15 +0400

    Made the code more beautiful ^_^

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Ui(QtWidgets.QDialog, Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.pushButton_2.clicked.connect(self.RDevice)
+        self.pushButton_2.clicked.connect(self.RDevice_calc)
         self.pushButton.clicked.connect(self.RWire_calc)
 
 
@@ -25,7 +25,7 @@ class Ui(QtWidgets.QDialog, Form):
 
             else:  # расчет по диаметру
                 cross = float(self.lineEdit_2.text())
-                resistance = ((1.6e-8) * lenght) / (0.785 * cross**2 / 1e6)  # R = (p * l) / S
+                resistance = ((1.6e-8) * lenght) / (0.785 * cross**2 / 1e6)  # R = (p * l) / (0.785 * D^2)
 
             self.label_5.setText(f'{resistance:.6f}')
 
@@ -33,44 +33,16 @@ class Ui(QtWidgets.QDialog, Form):
             self.label_5.setText(f'{err}')
 
 
+    def RDevice_calc(self):
+        try:
+            power = float(self.lineEdit_3.text())
+            voltage = float(self.lineEdit_4.text())
+            amperage = power / voltage                     # I = P / U
+            resistance = voltage / amperage                # R = U / I
+            self.label_13.setText(f'{resistance:.6f}')
 
-    def RDevice(self):
-        global a
-        global b
-        global c
-        a = float(self.lineEdit_3.text())
-        b = float(self.lineEdit_4.text())
-        c = a / b
-        d = b / c
-        d = ("%.6f" % d)
-        i = f'{d}'
-        self.label_13.setText(i)
-
-
-
-
-
-
-
-'''    def onMyButtonClick_2(self):
-        global otvet
-        if otvet == "0":
-            self.label_2.setText("Ответ не выбран")
-        elif otvet == "rb1":
-            self.label_2.setText("Верно")
-            self.pushButton_2.setStyleSheet('QPushButton {background-color: green}')
-        elif otvet == "rb2":
-            self.label_2.setText("Не верно")
-            self.pushButton_2.setStyleSheet('QPushButton {background-color: red}')
-        elif otvet == "rb3":
-            self.label_2.setText("Не верно")
-            self.pushButton_2.setStyleSheet('QPushButton {background-color: red}')
-
-    def choise(self, button):
-        global otvet
-        otvet = button.objectName()'''
-
-
+        except Exception as err:
+            self.label_13.setText(f'{err}')
 
 
 if __name__ == '__main__':
